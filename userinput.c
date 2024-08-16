46cca29172c3918834f76553ee388e9df5923017
bahguna200125 <amitbahuguna55@gmail.com>
2024-08-16 11:13:54 +0530

    basic c

---
+++ userinput.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main(){
+int a;
+scanf("%d",&a);
+// printf(" value of a =%d \n", a);
+// for (int i = 1; i <=a; i++)
+{
+    if (a==1)
+    {
+      printf( "it's raining ");
+    }
+      if (a==2)
+      {
+          printf( "it's not raining ");
+      }
+      else if (a>=3)
+      {
+          printf("%s", "it's wrong input");
+      }
+    
+    }
+   
+    }
+//    printf("%d \n",i);
+
+
+
